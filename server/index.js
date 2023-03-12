45b3cfdeac2945226be602d79ef9b8999e63d11a
Malika <alinurbe@gmail.com>
2023-03-12 01:09:39 -0800

    docker working

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 const { orderValidationRules, validate } = require('./validator.js')
 
